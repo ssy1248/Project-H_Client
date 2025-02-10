73890511c8d626aa8682d2682f250af521935adc
bakfox <153157605+bakfox@users.noreply.github.com>
2025-02-10 11:52:02 +0900

    fix : 디스폰 중복 삭제

--- Assets/Scripts/Town/TownManager.cs
+++ Assets/Scripts/Town/TownManager.cs
@@ -276,11 +276,6 @@ public class TownManager : MonoBehaviour
         errorText.GetComponent<TextMeshProUGUI>().SetText(data.Player.ToString());
     }
 
-    // 나가면 삭제해주기 
-    public void Despawn()
-    {
-
-    }
     //private Dictionary<int, Player> playerList = new();
     // 내가 마을에 참가하면 for문이든 반복문이든 돌리면서 생성해주기.
     public void AllSpawn(S_Spawn data)
