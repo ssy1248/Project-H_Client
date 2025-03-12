5e41fa92f5dfed7a4e536a0f9e9a7c119f3106a5
Hwang20241024 <ghkdaksdbs@naver.com>
2025-03-13 00:45:58 +0900

    feat: 보스몬스터 1차

--- Assets/Scripts/dungeon/BossManager.cs
+++ Assets/Scripts/dungeon/BossManager.cs
@@ -1,13 +1,117 @@
+using Google.Protobuf.Protocol;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BossManager : MonoBehaviour
 {
+    private static BossManager _instance = null;
+    public static BossManager Instance => _instance;
+
+    public GameObject demonKingPrefab;  // 보스 프리팹
+    private GameObject currentBoss;
+    private BossController boss;
+
+    public GameObject bladePoolPrefab;  // 블레이드 프리팹
+    private GameObject currentBladePool;
+
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void SpawnBoss(S_BossSpawn bossSpawnPacket)
+    {
+        Vector3 currentPosition;
+        currentPosition.x = bossSpawnPacket.CurrentPosition.X;
+        currentPosition.y = bossSpawnPacket.CurrentPosition.Y;
+        currentPosition.z = bossSpawnPacket.CurrentPosition.Z;
+
+
+        // 블레이드 생성
+        if (bladePoolPrefab != null)
+        {
+            currentBladePool = Instantiate(bladePoolPrefab, new Vector3(2, 0, 0), Quaternion.identity);
+        }
+
+        // 보스 몬스터 생성
+        if (demonKingPrefab != null)
+        {
+            currentBoss = Instantiate(demonKingPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            boss = currentBoss.GetComponent<BossController>();
+            boss.SetId(bossSpawnPacket.BossId);
+
+        }
+
+       
+
+
+        if (currentBoss != null )
+        {
+            currentBoss.transform.position = currentPosition;
+            currentBoss.transform.localScale = new Vector3(4f, 4f, 4f);
+        }
+    }
+
+    public void MoveBoss(S_BossMove bossMovePacket)
+    {
+        Debug.Log("여긴 오셔야합니다.");
+        Vector3 targetPosition = new Vector3(bossMovePacket.TargetPosition.X, bossMovePacket.TargetPosition.Y, bossMovePacket.TargetPosition.Z);
+        Debug.Log($"서버에서받은 보스 목표 패킷 : {targetPosition}");
+        if (boss != null)
+        {
+            boss.handleBossMove(targetPosition);
+        }
+        else
+        {
+            Debug.LogWarning("BossController가 없습니다.");
+        }
+
+    }
+
+    public void OnBossDeath()
+    {
+        currentBoss.SetActive(false);
+        currentBladePool.SetActive(false);
+    }
+
+    void TakeDamage()
+    {
+
+    }
+
+    public void ReceiveSkillPacket(S_BossSkillStart bossSkillStartPacket)
+    {
+        Debug.Log($"보스 스킬은 : {bossSkillStartPacket.Type}");
+        if (boss != null)
+        {
+            boss.handleBossSkill(bossSkillStartPacket.Type);
+        }
+        else
+        {
+            Debug.LogWarning("BossController가 없습니다.");
+        }
+
+
     }
 
     // Update is called once per frame
